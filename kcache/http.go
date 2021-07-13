8a730e49a17a27c313ad44ffe9f1e915dff802e0
Karlven <karlven@outlook.com>
2021-07-13 21:20:05 +0800

    finish http

--- kcache/http.go
+++ kcache/http.go
@@ -2,12 +2,17 @@ package kcache
 
 import (
 	"fmt"
+	"github.com/KarlvenK/kcache/consistenthash"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
+	"sync"
 )
 
 const defaultBasePath = "/_kcache/"
+const defaultReplicas = 50
 
 //HTTPPool implements PeerPicker for a pool of HTTP peers
 type HTTPPool struct {
@@ -16,6 +21,14 @@ type HTTPPool struct {
 	basePath string //作为节点间通讯地址的前缀，默认是 /_kcache/
 	//那么 http://example.com/_kcache/ 开头的请求，就用于节点间的访问。
 	//因为一个主机上还可能承载其他的服务，加一段 Path 是一个好习惯。比如，大部分网站的 API 接口，一般以 /api 作为前缀。
+
+	mu          sync.Mutex //guards peers and httpGetters
+	peers       *consistenthash.Map
+	httpGetters map[string]*httpGetter //keyed by e.g. "http://10.0.0.2:8008"
+}
+
+type httpGetter struct {
+	baseURL string
 }
 
 /*
@@ -73,3 +86,55 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/octet-stream")
 	_, _ = w.Write(view.ByteSlice())
 }
+
+//Set updates the pool's list of peers
+//实例化了一致性哈希算法，并且添加了传入的节点
+func (p *HTTPPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetters = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetters[peer] = &httpGetter{
+			baseURL: peer + p.basePath,
+		}
+	}
+}
+
+//PickPeer picks a peer according to key
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetters[peer], true
+	}
+	return nil, false
+}
+
+func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+	u := fmt.Sprintf("%v%v/%v", h.baseURL, url.QueryEscape(group), url.QueryEscape(key))
+	res, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = res.Body.Close()
+	}()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", err)
+	}
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading response body: %v", err)
+	}
+
+	return bytes, nil
+}
+
+var _ PeerGetter = (*httpGetter)(nil)
--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
+/*
+$ curl "http://localhost:9999/api?key=Tom"
+630
+$ curl "http://localhost:9999/api?key=kkk"
+kkk not exist
+*/
+
 import (
+	"flag"
 	"fmt"
 	"github.com/KarlvenK/kcache"
 	"log"
@@ -8,13 +16,13 @@ import (
 )
 
 var db = map[string]string{
-	"Tom":  "1",
-	"Jack": "2",
-	"Sam":  "3",
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
 }
 
-func main() {
-	kcache.NewGroup("int", 2<<10, kcache.GetterFunc(
+func createGroup() *kcache.Group {
+	return kcache.NewGroup("scores", 2<<10, kcache.GetterFunc(
 		func(key string) ([]byte, error) {
 			log.Println("[SlowDB] search key", key)
 			if v, ok := db[key]; ok {
@@ -22,9 +30,56 @@ func main() {
 			}
 			return nil, fmt.Errorf("%s not exist", key)
 		}))
+}
 
-	addr := "localhost:9999"
+func startCacheServer(addr string, addrs []string, gee *kcache.Group) {
 	peers := kcache.NewHTTPPool(addr)
+	peers.Set(addrs...)
+	gee.RegisterPeers(peers)
 	log.Println("kcache is running at", addr)
-	log.Fatal(http.ListenAndServe(addr, peers))
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
+}
+
+func startAPIServer(apiAddr string, gee *kcache.Group) {
+	http.Handle("/api", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			key := r.URL.Query().Get("key")
+			view, err := gee.Get(key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Write(view.ByteSlice())
+
+		}))
+	log.Println("fontend server is running at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+
+}
+
+func main() {
+	var port int
+	var api bool
+	flag.IntVar(&port, "port", 8001, "kcache server port")
+	flag.BoolVar(&api, "api", false, "Start a api server?")
+	flag.Parse()
+
+	apiAddr := "http://localhost:9999"
+	addrMap := map[int]string{
+		8001: "http://localhost:8001",
+		8002: "http://localhost:8002",
+		8003: "http://localhost:8003",
+	}
+
+	var addrs []string
+	for _, v := range addrMap {
+		addrs = append(addrs, v)
+	}
+
+	gee := createGroup()
+	if api {
+		go startAPIServer(apiAddr, gee)
+	}
+	startCacheServer(addrMap[port], addrs, gee)
 }
